2396285debaac458258d29e76501b141f09400de
nsindittngcobo-wq <nsindittngcobo@gmail.com>
2026-04-07 03:57:22 +0200

    Updated the UX

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ apiKey = "ec8t62ba8b244884fo89d6da5a743070";
 let prompt = `Generate a poem about ${promptElement.value}`;
 let context = "You are a very creative writer with unique ideas. Write a 5 line poem as prompted in basic HTML. Separate each line with a <br/>. Be short and precise. Do not include a title or markdown code blocks.";
 
+let poemElement = document.querySelector("#poem-content");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML =` 💭 Generating a poem about ${promptElement.value}`;
+
 apiURL =
   `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
